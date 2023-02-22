57d9b6837ac1cb9acf7dbb08bb97818ea8133bda
George Harrison Jr <35378163+markkiinotes@users.noreply.github.com>
2023-02-22 13:08:05 -0500

    Add files via upload

---
+++ Paint1.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * @author Harrison
+ */
+import java.util.Scanner;
+
+public class Paint1 {
+
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+        double wallHeight = 0.0;
+        double wallWidth = 0.0;
+        double wallArea = 0.0;
+        double gallonsPaintNeeded = 0.0;
+        double cansNeeded;
+
+        final double squareFeetPerGallons = 350.0;
+        final double gallonsPerCan = 1.0;
+
+        // Prompt user to input wall's height
+
+        System.out.println("Enter wall height (feet): ");
+        wallHeight = scnr.nextDouble();
+
+
+        // Prompt user to input wall's width
+        System.out.println("Enter wall width (feet): ");
+        wallWidth = scnr.nextDouble();
+        
+        while ((wallHeight < 1) || (wallWidth < 1)){
+                System.out.println("Error: Invalid entry.\n\n");
+                System.out.println("Enter wall height (feet): ");
+                wallHeight = scnr.nextDouble();
+
+        // Prompt user to input wall's width
+                System.out.println("Enter wall width (feet): ");
+                wallWidth = scnr.nextDouble();
+        }
+        
+        // Calculate and output wall area
+        wallArea = wallHeight * wallWidth;
+        System.out.println("Wall area:  " + wallArea + " square feet");
+
+        // Calculate and output the amount of paint in gallons needed to paint the wall
+        gallonsPaintNeeded = wallArea / squareFeetPerGallons;
+        System.out.println("Paint needed: " + gallonsPaintNeeded + " gallons");
+
+        // Calculate and output the number of paint cans needed to paint the wall,
+        // rounded up to nearest integer
+        // Complete this code block
+        cansNeeded = Math.ceil(gallonsPaintNeeded/gallonsPerCan);
+        
+        System.out.println("Cans needed: " + cansNeeded + " cans");
+        
+    }
+}
+
